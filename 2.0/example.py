f55937bdd16945dfc40c30062400614698ad5054
ashleeee <ashlee0125@outlook.com>
2017-03-06 00:15:42 +0900

    Add files via upload

---
+++ 2.0/example.py
@@ -0,0 +1,21 @@
+﻿
+# coding: utf-8
+
+import monapi
+
+mona = monapi.monapi()
+
+def init() :
+	mona.setroom(30)
+	mona.setpos(200, 200)
+	return
+
+def main() :
+	init()
+	mona.connect()
+	while(1) :
+		mona.update()
+		print(mona.recv())
+	return
+
+if __name__ == '__main__' : main()
